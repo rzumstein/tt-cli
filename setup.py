89d225f7226298fd6513e171ce3ea8a09b86a8dc
Ryan Zumstein <ryan.zumstein@tasconline.com>
2020-03-12 10:35:35 -0500

    adding metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup
 
 setup(
 	name='tt-cli',
+	description='A command line tool for Interval\'s Time Task software',
+	author='Ryan Zumstein',
+	author_email='b57364tc2re@opayq.com',
 	version='0.1.0',
-	scripts=['tt']
+	scripts=['tt'],
+	requires=['dotenv']
 )
